60bde9b91317a9b92970de6a4bb8504183d88b55
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-08-16 14:17:29 -0400

    Test por horario e idioma

--- src/kbot/kbot.test.js
+++ src/kbot/kbot.test.js
@@ -40,9 +40,9 @@ describe("Saludar por nombre", () => {
     });
 
     
-  });
+});
 
-  describe("Saludar tomando en cuenta la hora", () => {
+describe("Saludar tomando en cuenta la hora en idioma defaut", () => {
     it("deberia Saludar a Andrés en la mañana ", () => {
         let kbot = new Kbot();
         let resultado = kbot.saludar("Andrés", 7) 
@@ -60,5 +60,43 @@ describe("Saludar por nombre", () => {
         let resultado = kbot.saludar("Andrés", 21) 
         expect(resultado).toEqual("Buenas noches Andrés");
     });
+});
 
-  });
+describe("Saludar tomando en cuenta la hora y por idioma ingles", () => {
+    it("deberia Saludar a Andrés en la mañana en español ", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés", 7) 
+        expect(resultado).toEqual("Buenos días Andrés");
+    });
+
+    it("deberia Saludar a Andrés en la tarde en español", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés", 15) 
+        expect(resultado).toEqual("Buenas tardes Andrés");
+    });
+
+    it("deberia Saludar a Andrés en la noche en español", () => {
+        let kbot = new Kbot("ES");
+        let resultado = kbot.saludar("Andrés", 21) 
+        expect(resultado).toEqual("Buenas noches Andrés");
+    });
+
+
+    it("deberia Saludar a Andrés en la mañana en ingles ", () => {
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés", 7) 
+        expect(resultado).toEqual("Good morning Andrés");
+    });
+
+    it("deberia Saludar a Andrés en la tarde en ingles", () => {
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés", 15) 
+        expect(resultado).toEqual("Good afternoon Andrés");
+    });
+
+    it("deberia Saludar a Andrés en la noche en ingles", () => {
+        let kbot = new Kbot("EN");
+        let resultado = kbot.saludar("Andrés", 21) 
+        expect(resultado).toEqual("Good evening Andrés");
+   });
+});
